95a229f4fd47342292059470906b1285d15fe5ef
David Yu <lewen.yu@gmail.com>
2015-11-29 21:03:27 -0800

    [HX] add doc-generating helpers

---
+++ main.hx
@@ -0,0 +1,16 @@
+import gml.Angle;
+import gml.Nat;
+import gml.matrix.Matrix;
+import gml.matrix.Mat2;
+import gml.matrix.Mat3;
+import gml.matrix.Mat4;
+import gml.vector.Vec;
+import gml.vector.Vec2f;
+import gml.vector.Vec3f;
+import gml.vector.Vec4f;
+
+class Main {
+    static function main() {
+        trace("Hello World !");
+    }
+}
